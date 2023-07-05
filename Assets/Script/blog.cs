c9169571f7387717db89b0fad829204d3b6092f1
kano3 <1354427585@qq.com>
2023-07-05 15:51:43 +0800

    初始化项目

---
+++ Assets/Script/blog.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blog : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		Debug.Log("function Start()");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Debug.Log("function Update()");
+	}
+
+	void FixedUpdate()
+	{
+		Debug.Log("function FixedUpdate()");
+		Update();
+	}
+}
