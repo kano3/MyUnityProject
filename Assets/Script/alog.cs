14f3302442647a34e329e176a0fbae9377b0bab9
kano3 <1354427585@qq.com>
2023-07-05 15:56:09 +0800

    初始化项目

--- Assets/Script/alog.cs
+++ Assets/Script/alog.cs
@@ -7,18 +7,12 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("function Start()");
+        Debug.Log("hello world!");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("function Update()");
-    }
-
     void FixedUpdate()
     {
         Debug.Log("function FixedUpdate()");
-        Update();
     }
 }
